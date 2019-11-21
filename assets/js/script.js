3a17f0352f5100181a44a2dce19597e6154ba2dd
Matthew4Jesus <matvii.mashtalyar@gmail.com>
2019-11-21 22:29:49 +0300

    added slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
+
 $('.menu-btn').on('click', function(e) {
 	e.preventDefault;
 	$(this).toggleClass('active');
 	$('.menu__box').toggleClass('active');
 })
+
+
+$('.reviews__content').slick({
+		slidesToShow: 2,
+		responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 1,
+	        dots: true,
+	        arrows: false
+	      }
+	    } 
+]
+	}); 
