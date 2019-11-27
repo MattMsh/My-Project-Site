89a0ea194fa8b1f32da2c655a34f9c9302e55a5f
Matthew4Jesus <matvii.mashtalyar@gmail.com>
2019-11-27 13:30:33 +0300

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,28 @@ $('.reviews__content').slick({
 	    } 
 ]
 	}); 
+
+
+$('.menu__item').on( 'click', function(){ 
+    var el = $(this);
+    var dest = el.attr('href'); // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top+1 // прокручиваем страницу к требуемому элементу
+        }, 500 // скорость прокрутки
+        );
+    }
+    return false;
+});
+
+$('.footer__nav-link').on( 'click', function(){ 
+    var el = $(this);
+    var dest = el.attr('href'); // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top+1 // прокручиваем страницу к требуемому элементу
+        }, 500 // скорость прокрутки
+        );
+    }
+    return false;
+});
